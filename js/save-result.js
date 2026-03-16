2972392c0e8fd98723661a37927815b038979bf8
Stateman2109 <statemantech@gmail.com>
2026-03-16 19:04:06 +0100

    fffff

--- js/save-result.js
+++ js/save-result.js
@@ -1,3 +1,7 @@
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqaXFrZHFxZnZ1bG1ha214dG1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NzY0NzcsImV4cCI6MjA4OTI1MjQ3N30.q0qlipCmr8OfbnpiOEE8E4eFJgRJ9YYUGqlOaUmIRW8";
+const SUPABASE_URL = "https://ejiqkdqqfvulmakmxtmb.supabase.co";
+
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function saveResult(data) {
